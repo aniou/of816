b12a62d8b53a6e75463f3a8c428873539317b441
Piotr Meyer <aniou@smutek.pl>
2020-03-22 19:53:31 +0100

    C256 Foenix port

---
+++ platforms/C256/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e -x
+export PATH="$HOME/bin:$PATH"
+
+ca65 -I ../../inc C256.s -l C256.lst
+../../build.sh C256
+ld65 -v -C C256.l -S 0x2000 C256.o ../../forth.o -m forth.map -o forth
+srec_cat forth -binary -offset 0x3a0000 -o forth.hex -intel
+
+ls -l forth *.hex
