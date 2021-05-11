cc52389d62ada7c6bc2c518970faf5548c1f19cd
Piotr Meyer <aniou@smutek.pl>
2021-05-11 18:47:46 +0200

    update build.sh and re-gen with cc65 v2.19

--- platforms/C256/build.sh
+++ platforms/C256/build.sh
@@ -5,7 +5,7 @@ export PATH="$HOME/bin:$PATH"
 toke fcode/xmodem.fs
 toke fcode/ansi.fs
 toke fcode/editor.fs
-ca65 -I ../../inc C256.s -l C256.lst
+ca65 -I inc -I ../../inc C256.s -l C256.lst
 ../../build.sh C256
 ld65 -v -C C256.l C256.o ../../forth.o -m forth.map -o forth
 
